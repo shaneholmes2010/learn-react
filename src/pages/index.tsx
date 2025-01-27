fee9a7133118104de717ba610303e1fb545e3e99
shaneholmes2010 <shaneholmes2010@gmail.com>
2025-01-27 12:30:23 -0500

    modified state to handle error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,11 @@
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
-import Gallery from './qcomps/gallery_props'
 import List from './qcomps/list_keys_id'
-import RecipeList from './qcomps/recipes'
+import Gallery from './qcomps/state'
 
 export default function Home() {
   return (
-    <RecipeList />
+    <Gallery />
   )
 }
